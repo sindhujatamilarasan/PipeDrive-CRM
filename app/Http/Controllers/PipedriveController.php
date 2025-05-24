2679814e380ca9643c6ae463ac33e56f64a6d7b8
Sindhujakiluvai256 <sindhujatkiluvaitech@gmail.com>
2025-05-24 15:43:34 +0530

    Configured stripe data in laravel updated two

--- app/Http/Controllers/PipedriveController.php
+++ app/Http/Controllers/PipedriveController.php
@@ -116,7 +116,7 @@ public function showPanel(Request $request)
     // Create an HTML output string with data rendered inside tags, like a table or list
     $html = '<h3>Stripe Invoice Data</h3><ul>';
     Log::info('Stripe API Response:', ['response' => $data]);
-    foreach ($data['result']['invoices'] as $invoice) {
+    foreach ($data['invoices'] as $invoice) {
         $html .= '<li>Invoice ID: ' . htmlspecialchars($invoice['id']) . '<br>';
         $html .= 'Paid: ' . htmlspecialchars($invoice['amount_paid']) . '<br>';
         $html .= 'Amount Due: ' . htmlspecialchars($invoice['amount_due']) . '</li><br>';
